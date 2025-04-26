ef762d6afab7ad2ad5554f0aad27496d2ab345da
Pritom07 <pritommohajon4545@gmail.com>
2025-04-26 12:36:49 +0600

    delete operation performed in jobApplications collections

--- index.js
+++ index.js
@@ -109,6 +109,13 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/jobApplications/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await jobApplicationsCollection.deleteOne(query);
+      res.send(result);
+    });
+
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
