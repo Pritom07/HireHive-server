2cea3cd4d44a69a0763eb8088a9c978e523739a0
Pritom07 <pritommohajon4545@gmail.com>
2025-04-27 22:20:31 +0600

    Calculating total applicant when a person apply a job and update total applicant number when a person remove his/her job application & also showing the all posted jobs that was posted by a specific person in the UI

--- index.js
+++ index.js
@@ -81,7 +81,12 @@ async function run() {
     });
 
     app.get("/jobs", async (req, res) => {
-      const cursor = jobsCollection.find();
+      const email = req.query.email;
+      let query = {};
+      if (email) {
+        query = { hr_email: email };
+      }
+      const cursor = jobsCollection.find(query);
       const result = await cursor.toArray();
       res.send(result);
     });
@@ -95,6 +100,21 @@ async function run() {
 
     app.post("/jobApplications", async (req, res) => {
       const applicantInfo = req.body;
+      const jobId = applicantInfo.jobId;
+      const query = { _id: new ObjectId(jobId) };
+      const desiredJob = await jobsCollection.findOne(query);
+      let newCount = 0;
+      if (desiredJob.applicantCount) {
+        newCount = desiredJob.applicantCount + 1;
+      } else {
+        newCount = 1;
+      }
+      const updateDoc = {
+        $set: {
+          applicantCount: newCount,
+        },
+      };
+      const updatedResult = await jobsCollection.updateOne(query, updateDoc);
       const result = await jobApplicationsCollection.insertOne(applicantInfo);
       res.send(result);
     });
@@ -118,7 +138,57 @@ async function run() {
     app.delete("/jobApplications/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
+      const findingDeletedjob = await jobApplicationsCollection.findOne(query);
+      const jobId = findingDeletedjob.jobId;
+      const query2 = { _id: new ObjectId(jobId) };
+      const desiredJob = await jobsCollection.findOne(query2);
+      let newCount = 0;
+      if (desiredJob.applicantCount) {
+        newCount = desiredJob.applicantCount - 1;
+      } else {
+        newCount = 0;
+      }
+
+      const updateDoc = {
+        $set: {
+          applicantCount: newCount,
+        },
+      };
+      const updatedResult = await jobsCollection.updateOne(query2, updateDoc);
       const result = await jobApplicationsCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    app.get("/jobApplications/:jobID", async (req, res) => {
+      const jobID = req.params.jobID;
+      const query = { jobId: jobID };
+      const cursor = jobApplicationsCollection.find(query);
+      const result = await cursor.toArray();
+      for (let jobDocument of result) {
+        const jobId = jobDocument.jobId;
+        const query = { _id: new ObjectId(jobId) };
+        const desiredJob = await jobsCollection.findOne(query);
+        jobDocument.title = desiredJob.title;
+      }
+      res.send(result);
+    });
+
+    app.put("/jobApplications", async (req, res) => {
+      const Status = req.body.status;
+      const jobID = req.query.jobID;
+      const email = req.query.email;
+      const filter = { jobID, email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          status: Status,
+        },
+      };
+      const result = await jobApplicationsCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
       res.send(result);
     });
 
